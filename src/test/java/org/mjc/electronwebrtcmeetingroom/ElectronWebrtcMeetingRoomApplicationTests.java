17af1efe3496e9675f05955f40b0c41b82b13187
mojucheng <mojucheng@bytedance.com>
2021-07-15 09:14:42 +0800

    init: webrtc meeting login server

---
+++ src/test/java/org/mjc/electronwebrtcmeetingroom/ElectronWebrtcMeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjc.electronwebrtcmeetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronWebrtcMeetingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
